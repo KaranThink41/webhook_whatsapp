5a09b4d0e95a5c974aa5f9d719e93e7ba98a0427
KaranThink41 <karan@gmail.com>
2025-07-03 21:36:49 +0530

    api changes

--- modules/config.js
+++ modules/config.js
@@ -1,8 +1,8 @@
 // Configuration
 module.exports = {
-  WHATSAPP_TOKEN: process.env.WHATSAPP_TOKEN,
-  WHATSAPP_PHONE_NUMBER_ID: process.env.WHATSAPP_PHONE_NUMBER_ID,
-  WEBHOOK_VERIFY_TOKEN: process.env.WEBHOOK_VERIFY_TOKEN,
-  DJANGO_BASE_URL: process.env.DJANGO_BASE_URL,
-  WHATSAPP_API_URL: `https://graph.facebook.com/v18.0/${process.env.WHATSAPP_PHONE_NUMBER_ID}/messages`
+  WHATSAPP_TOKEN: process.env.WHATSAPP_TOKEN || '',
+  WHATSAPP_PHONE_NUMBER_ID: process.env.WHATSAPP_PHONE_NUMBER_ID || '',
+  WEBHOOK_VERIFY_TOKEN: process.env.WEBHOOK_VERIFY_TOKEN || 'default_webhook_token',
+  DJANGO_BASE_URL: process.env.DJANGO_BASE_URL || 'http://localhost:8000',
+  WHATSAPP_API_URL: `https://graph.facebook.com/v18.0/${process.env.WHATSAPP_PHONE_NUMBER_ID || 'YOUR_PHONE_NUMBER_ID'}/messages`
 };
